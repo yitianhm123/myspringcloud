5f99f3b97abfe667785c4d8ea861aaa5398dad87
huangman <595710882@qq.com>
2018-08-22 16:36:16 +0800

    mycode

---
+++ eureka/src/test/java/hmspringcloud/eureka/EurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package hmspringcloud.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.estate.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
