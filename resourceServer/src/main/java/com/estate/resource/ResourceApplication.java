a3fe96ab498b8db7922536c4909a6c76f9d0e612
huangman <595710882@qq.com>
2018-08-23 17:31:37 +0800

    zuul

--- resourceServer/src/main/java/com/estate/resource/ResourceApplication.java
+++ resourceServer/src/main/java/com/estate/resource/ResourceApplication.java
@@ -2,8 +2,13 @@ package com.estate.resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@EnableZuulProxy
+@ComponentScan
 public class ResourceApplication {
 
 	public static void main(String[] args) {
